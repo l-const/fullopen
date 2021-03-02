42df5d3d1dff0079c3848f7cc7c7b342d090882d
l-const <const-l@outlook.com>
2021-03-02 10:59:55 +0200

    part3a fin

---
+++ ch3/step3/index.js
@@ -0,0 +1,47 @@
+const { request, response } = require("express")
+const express = require("express")
+const app = express()
+
+let persons = [
+    {id:1, name: "Arto Hellas", number: "040-123456"},
+    {id:2, name: "Ada Lovelace", number: "39-44-5323523"},
+    {id:3, name: "Dan Abramov", number: "12-43-234345"},
+    {id:4, name: "Mary Poppendick", number: "39-23-6423122"},
+]
+
+app.get("/", (request, response) => {
+    response.send("<h1>Hello World</h1>")
+})
+
+
+app.get("/info", (request, response) => {
+    const time = new Date()
+    const p_len = persons.length
+    const html = `<p>Phonebook has info for ${p_len} people <p>${time}</p></p>`
+    response.send(html)
+
+})
+
+
+app.get("/api/persons", (request, response) => {
+    response.json(persons)
+})
+
+
+app.get("/api/persons/:id", (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(p => p.id === id)
+    if(person){
+        response.json(person)
+    } else {
+        response.status(404).end
+    }
+})
+
+const PORT=3001 
+app.listen(PORT, () => {
+    console.log(`Server listening at port ${PORT}`)
+    console.log(`\nIndex  @ http://localhost:${PORT}`)
+})
+
+
